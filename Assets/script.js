efd0211fb6ab99efb0f8c76b91d6205b5abd0915
JTJasso <109089683+JTJasso@users.noreply.github.com>
2023-06-15 20:39:45 -0600

    updated webpage to stop colosplosion

--- Assets/script.js
+++ Assets/script.js
@@ -8,6 +8,19 @@ const btn1 = document.getElementById("btn-1");
 const btn2 = document.getElementById("btn-2");
 const btn3 = document.getElementById("btn-3");
 const btn4 = document.getElementById("btn-4");
+const submit = document.getElementById("submit");
+const highscore_div = document.getElementById("highscore_div");
+const olelement = document.getElementById("highscore_reset");
+const highscore_list = document.getElementById("highscore_list");
+
+
+const gamertag = document.getElementById("gamertag");
+
+
+
+
+
+
 
 btn1.addEventListener("click", checkanswer);
 btn2.addEventListener("click", checkanswer);
@@ -46,6 +59,8 @@ const questionsDB = [
 
 let currentQuestion = 0;
 let score = 0;
+// let gamername = [];
+let finalscore = 0;
 let secondsLeft = 90;
 
 startbtn.addEventListener("click", startQuiz);
@@ -73,13 +88,10 @@ function setTime() {
     // Sets interval in variable
     timerInterval = setInterval(function () {
         secondsLeft--;
-        timerelement.textContent = secondsLeft + " seconds left till colorsplosion.";
+        timerelement.textContent = secondsLeft + " seconds left";
 
         if (secondsLeft <= 0) {
-            // Stops execution of action at set interval
-            clearInterval(timerInterval);
-            // Calls function to create and append image
-            sendMessage();
+           endQuiz();
         } else {
             secondsLeft--
         }
@@ -102,6 +114,61 @@ function checkanswer() {
         currentQuestion++;
         revealQuestion();
     } else {
-
+        // localStorage.setItem("Subscore", JSON.stringify(secondsLeft));
+        endQuiz();
     }
+}
+
+
+
+function endQuiz() {
+    quiz.classList.add("hide");
+    clearInterval(timerInterval);
+    finalscore = secondsLeft;
+    console.log("finalscore", finalscore);
+
+    gamertag.value = "";
+    
+    submit.addEventListener("click", () => {
+    
+        olelement.removechild(li);
+        
+    });
+
+    submit.addEventListener("click", () => {
+    //    console.log("removed", highscore_list.remove());
+    //    highscore_list.removeChild(li);
+        //Sets gamertag and score to local storage
+    localStorage.setItem(gamertag.value, JSON.stringify(finalscore));
+        //spreads local storage into an object
+        const items = { ...localStorage };
+        console.log("items", items)
+        const itemsarray = Object.entries(items)
+        // for each item in the object, create a list item and append it to the highscore list
+        itemsarray.forEach(completeditem => {
+            console.log(completeditem);
+            const li = document.createElement("li");
+            li.innerHTML = JSON.stringify(completeditem);
+            olelement.appendChild(li);
+            
+            
+        });
+        
+        
+        const li = document.createElement("li");
+        li.innerHTML = JSON.stringify(completeditem);
+        highscore_list.appendChild(li);
+         return;
+    });
+
+    console.log("gamertag.value", gamertag.value);
+
+    highscore_div.classList.remove("hide");
+    var Subscore = localStorage.getItem("Subscore");
+    console.log(Subscore);
+
+    
+
+
+    
 }
